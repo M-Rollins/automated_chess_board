0a8845a66795970ff092300e3bf766f3c8cd083c
M-Rollins <mar289@case.edu>
2026-04-11 17:16:09 -0400

    Frist attempt at using IO board for control

---
+++ IO_manager.py
@@ -0,0 +1,158 @@
+# from gpiozero import PWMLED
+import gpiozero
+import threading
+import time
+from main import GameManager, Command
+
+
+# pin definitions
+button_pins = [11, 4, 10, 27]
+button_led_pins = [5, 17, 9, 22]
+toggle_sw_pins = [3, 13]
+toggle_led_pins = [2, 6]
+rot_sw_pins = [12, 16, 20, 21, 26, 19]
+
+# class InputData:
+#     def __init__(self):
+#         self.last_button = None
+#         self.rot_sw_value = 0
+#         self.keyboard_input = None
+
+class IO_Manager():
+    '''Class for controling LEDs, reading switch and button states, and communicating control inputs to the main program'''
+    def __init__(self, command_queue):
+        self.command_queue = command_queue
+        
+        numbuttons = 4
+        self.buttons = [None] * numbuttons
+        for i in range(numbuttons):
+            self.buttons[i] = MyButton(button_pins[i], button_led_pins[i], i, i + numbuttons)
+            
+        
+        self.white_toggle = ToggleSwitch(toggle_sw_pins[0], toggle_led_pins[0])
+        self.black_toggle = ToggleSwitch(toggle_sw_pins[0], toggle_led_pins[0])
+        self.toggle_states = (False, False)
+        
+        self.rot_sw = RotarySwitch(rot_sw_pins)
+        
+
+
+    def update_loop(self):
+        while True:
+            # read rotary switch
+            val = self.rot_sw.read()
+            if val != self.rot_sw_value:
+                self.rot_sw_value = val
+                self.command_queue.put(Command(GameManager.configure_engine, val, None))
+                
+            # read toggle switches
+            new_toggle_states = (self.white_toggle.read(), self.black_toggle.read())
+            if new_toggle_states != self.toggle_states:
+                self.toggle_states = new_toggle_states
+                self.command_queue.put(Command(GameManager.configure_players, self.toggle_states, None))
+            
+            # read pushbuttons
+            for b in self.buttons:
+                val = b.update()
+                if val != -1:
+                    self.command_queue.put(Command(GameManager.button_press, val, time.time() + 0.1))
+                
+class KeyboardManager():
+    def __init__(self, command_queue):
+        self.command_queue = command_queue
+    
+    def update_loop(self):
+        while(True):
+            s = input()
+            self.command_queue.put(Command(GameManager.keyboard_input, s, time.time() + 0.1))
+   
+        
+   
+class MyButton():
+    '''Class for controlling a pushbutton with integrated LED indicator. val1 is a value retuned on a short press, fn2 on a long press. The LED is ltoggled while one of the functions is in progress'''
+    def __init__(self, sw_pin, led_pin, val1, val2, led_default_state=False, long_press_threshold=0.5):
+        self.button = gpiozero.Button(sw_pin)
+        self.led = gpiozero.LED(led_pin)
+        self.led_default = led_default_state
+        self.val1 = val1
+        self.val2 = val2
+        self.sw_state = False
+        self.press_detected = False    # indicate when a long press has been triggered but the button hasn't been released
+        self.sw_press_time = 0
+        
+        self.set_led(self.led_default)
+    
+    def update(self):
+        '''Read switch state and return val1 when a short press is detected, val2 when a long press is detected, and -1 otherwise'''
+        if self.sw_state:
+            duration = time.time() - self.sw_press_time
+            
+            
+            # # retrun LED to default after the command finishes
+            # self.set_led(self.led_default)
+            
+            if not self.button.is_pressed:
+                self.sw_state = False
+                self.press_detected = False
+                
+                # if the button wasn't held, register a short press
+                if not self.press_detected:
+                    return self.val1
+                
+            elif duration > self.long_press_threshold:
+                self.press_detected = True
+                return self.val2
+                
+        else:
+            if self.button.is_pressed:
+                self.sw_state = True
+                self.sw_press_time = time.time()
+                # toggle LED when the button is pressed
+                self.set_led(not self.led_default)
+        return -1
+                
+    def set_led(self, state):
+        if state:
+            self.led.on()
+        else:
+            self.led.off()
+            
+            
+class ToggleSwitch():
+    '''Toggle switch with associated LED'''
+    def __init__(self, sw_pin, led_pin, led_default_state):
+        self.button = gpiozero.Button(sw_pin)
+        self.led = gpiozero.LED(led_pin)
+        self.led_default = led_default_state
+        
+        self.set_led(self.led_default)
+        
+    def read(self):
+        self.sw_state = self.button.pressed
+        self.set_led(self.sw_state)
+        return self.sw_state
+    
+    def set_led(self, state):
+        if state:
+            self.led.on()
+        else:
+            self.led.off()
+            
+            
+class RotarySwitch():
+    '''Reads state of multi-position rotary switch (each position connects a different pin to ground)'''
+    def __init__(self, pins):
+        self.num_positions = len(pins)
+        self.switches =[gpiozero.Button(p) for p in pins]
+        
+        
+    def read(self):
+        '''Return switch position in a range of 0-(n-1) where n is the number of positions'''
+        for i in range(self.num_positions):
+            if self.switches[i].is_pressed():
+                
+                return i
+        
+        # error state: none of the switches is closed
+        return -1
+    
--- main.py
+++ main.py
@@ -2,36 +2,212 @@ import chess
 import chess.engine
 import sys
 from board_adapter import BoardAdapter
+import IO_manager
+from collections import namedtuple
+import threading
 import time
+import queue
 
 
-class Game():
+# # engine skill level ranges from 0-20
+# engine_skill_levels = [0, 2, 5, 10, 15, 20]
+
+# settings for engine level (based on lichess skill settings 1, 3, 4, 5, 6, 8)
+engine_skill_levels = [-9, -1, 3, 7, 11, 20]
+engine_time_limits = [.05, .15, .2, .3, .4, 1]
+engine_depth = [5, 5, 5, 5, 8, 22]
+
+# Commands are put in a queue monitored by a GameManager. When a executing a command, 
+# it calls the given function with the provided argumens. If the current time is after
+# the expiration time, the command is skipped
+Command = namedtuple('Command', ['func', 'args', 'expiration_time'])
     
-    def __init__(self, robot, w=True, b=True, cpu_time_limit=0.1, engine_strength=20):
+
+class GameManager():
+    MENU = 1
+    AWAITING_MOVE = 2
+    BUSY = 3
+    
+    
+    
+    
+    
+    def __init__(self, robot, cpu_time_limit=0.1, engine_strength=20):
         self.robot = robot
-        self.white_is_human = w
-        self.black_is_human = b
+        self.white_is_cpu = False
+        self.black_is_cpu = False
         self.cpu_time_limit = cpu_time_limit
         self.engine_strength = engine_strength
         
-        self.engine = chess.engine.SimpleEngine.popen_uci('stockfish/stockfish-windows-x86-64-sse41-popcnt.exe')
+        self.engine_filepath = 'stockfish/stockfish-android-armv8'
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.engine_filepath)
         self.engine.configure({"Skill Level": self.engine_strength})
         self.board = chess.Board()
-        self.game_over = False
+        # self.game_over = False
+        
+        
+        
+        self.command_queue = queue.Queue()
+        
+        self.game_in_progress = False
+        self.state = self.MENU
+        # monitors switches/buttons and controls LEDs
+        self.IO = IO_manager.IO_Manager(self.command_queue)
+        self.keyboard = IO_manager.KeyboardManager(self.command_queue)
+        
+        
+        
     
-    def set_engine_strength(self, s):
-        self.engine_strength = s
+    def run(self):
+        # Start threads to monitor inputs
+        threading.Thread(target=self.IO.update_loop, daemon=True).start()
+        threading.Thread(target=self.keyboard.update_loop, daemon=True).start()
+        
+        while True:
+            # check for input from the control panel / keyboard
+            while not self.command_queue.empty():
+                cmd = self.command_queue.get()
+                if cmd.expiration_time is not None and cmd.expiration_time < time.time():
+                    continue
+                
+                # execute the command
+                if type(cmd.args) == tuple:
+                    cmd.function(*cmd.args)
+                else:
+                    cmd.function(cmd.args)
+            
+            
+            # If a game is running and it is the computer's turn, query the engine
+            if self.game_in_progress:
+                cpu_turn = self.white_is_cpu if self.board.turn else self.black_is_cpu
+                if cpu_turn:
+                    self.play_move(self.get_computer_move())
+                    if self.board.is_game_over():
+                        outcome = self.board.outcome()
+                        print(f'game over: {outcome}')
+                        self.quit_game()
+                        
+            # match self.state:
+            #     case MENU:
+            #         pass
+            #     case AWAITING_MOVE:
+            #         pass
+            #     case _:
+            #         pass
+        
+        
+    
+    # def set_engine_strength(self, s):
+    #     self.engine_strength = s
+        
+    def configure_engine(self, level):
+        '''Set the engine to one of six levels (0-5)'''
+        print(f'setting engine to level {level}')
+        self.engine.configure({"Skill Level": engine_skill_levels[level],
+                               "time": engine_time_limits[level],
+                               "depth": engine_depth[level]})
+    
+    def configure_players(self, white, black):
+        self.white_is_cpu = white
+        self.black_is_cpu = black
+    
+        
+    def keyboard_input(self, user_input):
+        '''Parse user input text commands'''
+        if self.state == self.BUSY:
+            print('busy')
+            return        
+        
+        # some commands may be followed by additional arguments, separetd by spaces
+        if ' ' in user_input:
+            [cmd, args] = user_input.lower().split(' ', maxsplit=1)
+        else:
+            cmd = user_input.lower()
+            args = ''
+            
+        match cmd:
+            case 'play':    # start a game, letting the user chose which side(s) to play
+                if self.state != self.MENU:
+                    print('\'play\' command is only allowed from the menu')
+                    return
+
+                # check that the board is set properly
+                if not robot.is_starting_position:
+                    print('\tboard is not in starting position (call \'setup\' to set up automatically or \'init\' to set up manually)')
+                    return
+                
+                white_is_human = 'w' in args
+                black_is_human = 'b' in args
+                self.configure_players(not white_is_human, not black_is_human)
+                # start the game
+                self.board = chess.Board()
+                self.state = self.AWAITING_MOVE
+                
+            case 'resume':    # resume the game in progress, optionally changing sides
+                if self.state != self.MENU:
+                    print('\'resume\' command is only allowed from the menu')
+                    return
+                if not self.game_in_progress:
+                    print('\tno game in progress')
+                    return
+                
+                # update which side(s) the computer plays
+                white_is_human = 'w' in args
+                black_is_human = 'b' in args
+                self.configure_players(not white_is_human, not black_is_human)
+                # reume the game
+                self.state = self.AWAITING_MOVE
+            
+            case 'engine':    # set the strength of the engine (0-5)
+                try:
+                    level = int(args)
+                    if(level < 0 or s > 5):
+                        print('Engine level must be an integer from 0-5')
+                    else:
+                        self.configure_engine(level)
+                        print('\tsetting engine level to', level)
+                except ValueError:
+                    print('Engine level must be an integer from 0-5')
+                
+            case 'init':    # initialize the board state to the starting position after manually setting up
+                robot.initialize_position()
+                self.game_in_progress = False
+                self.state = self.MENU
+            case 'setup':    # physically reset the board to the starting position
+                robot.setup_position('starting')
+                self.game_in_progress = False
+                self.state = self.MENU
+            case 'clear':    # physically remove all pieces from the board
+                robot.setup_position('empty')
+                self.game_in_progress = False
+                self.state = self.MENU
+            case 'adjust':
+                robot.adjust_pieces()
+            case 'home':
+                robot.home()
+            case 'exit':
+                sys.exit()
+            case _:
+                print('\tinvalid command')
+        
+    
+    
+    def button_press(self, button_id):
+        #TODO
+        print(f'button {button_id}')
+    
         
     def resume(self):
         '''Start up a game again after quitting'''
-        self.engine = chess.engine.SimpleEngine.popen_uci('stockfish/stockfish-windows-x86-64-sse41-popcnt.exe')
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.engine_filepath)
         self.engine.configure({"Skill Level": self.engine_strength})
         self.game_over = False
         self.play_game()
         
-    def quitGame(self):
+    def quit_game(self):
         self.engine.quit()
-        self.game_over = True
+        # self.game_over = True
+        self.game_in_progress = False
     
     def get_human_move(self):
         '''Allow a human player to choose a move. Stops the game if the "quit" command is recieved'''
@@ -58,31 +234,42 @@ class Game():
         return result.move
     
     
-    def play_game(self):
-        '''Play a game, ending when the game is over or the user enters the "quit" command'''
-        while not self.board.is_game_over():
-            move = chess.Move.null
-        
-            if (self.board.turn and self.white_is_human) or (not self.board.turn and self.black_is_human):
-                move = self.get_human_move()
-                if self.game_over:
-                    print('game ended by user')
-                    return
-            else:
-                move = self.get_computer_move()
-        
-            if move in self.board.legal_moves:
-                # TODO: handle ep captures
-                self.robot.move(move.uci(), is_castling=self.board.is_castling(move))
-                self.board.push(move)
-                
-            print(move)
-            print(self.board)
-            print()
+    def play_move(self, move):
+        if move in self.board.legal_moves:
+            # TODO: handle promotion and ep captures
+            self.robot.move(move.uci(), is_castling=self.board.is_castling(move))
+            self.board.push(move)
             
-        outcome = self.board.outcome()
-        print(f'game over: {outcome}')
-        self.quitGame()
+        print(move)
+        print(self.board)
+        print()
+        
+        
+    # def play_game(self):
+    #     '''Play a game, ending when the game is over or the user enters the "quit" command'''
+    #     while not self.board.is_game_over():
+    #         move = chess.Move.null
+        
+    #         if (self.board.turn and self.white_is_human) or (not self.board.turn and self.black_is_human):
+    #             move = self.get_human_move()
+    #             if self.game_over:
+    #                 print('game ended by user')
+    #                 return
+    #         else:
+    #             move = self.get_computer_move()
+        
+    #         if move in self.board.legal_moves:
+    #             # TODO: handle promotion and ep captures
+    #             self.robot.move(move.uci(), is_castling=self.board.is_castling(move))
+    #             self.board.push(move)
+                
+    #         print(move)
+    #         print(self.board)
+    #         print()
+            
+    #     outcome = self.board.outcome()
+    #     print(f'game over: {outcome}')
+    #     self.quitGame()
 
 
 if __name__ == '__main__':
@@ -94,77 +281,5 @@ if __name__ == '__main__':
     robot.home()
     robot.initialize_position()
     time.sleep(1)
-        
-    game = None
-    engine_strength = 20
     
-    while True:
-        # let the user choose which side(s) to play
-        user_input = input('command(\'exit\' to quit): ')
-        if ' ' in user_input:
-            [cmd, args] = user_input.lower().split(' ', maxsplit=1)
-        else:
-            cmd = user_input.lower()
-            args = ''
-        
-        match cmd:
-            case 'play':
-                # start a game, letting the user chose which side(s) to play
-                
-                # check that the board is set properly
-                if not robot.is_starting_position:
-                    print('\tboard is not in starting position (call \'setup\' to set up automatically or \'init\' to set up manually)')
-                    continue
-                
-                white_is_human = 'w' in args
-                black_is_human = 'b' in args
-                # start the game
-                game = Game(robot, white_is_human, black_is_human, engine_strength=engine_strength)
-                game.play_game()
-                
-            case 'resume':
-                # resume the game in progress, optionally changing sides
-                if game is None:
-                    print('\tno game in progress')
-                    continue
-                
-                # update which side(s) the computer plays
-                game.white_is_human = 'w' in args
-                game.black_is_human = 'b' in args
-                
-                game.resume()
-            
-            case 'engine':
-                # set the strength of the engine (1-20)
-                try:
-                    s = int(args)
-                    if(s < 0 or s > 20):
-                        print('Engine strength must be an integer from 0-20')
-                    else:
-                        engine_strength = s
-                        if game is not None: game.set_engine_strength(s)
-                        print('\tsetting engine strength to', s)
-                except ValueError:
-                    print('Engine strength must be an integer from 0-20')
-                
-                
-            case 'init':
-                # initialize the board state to the starting position after manually setting up
-                robot.initialize_position()
-                game = None
-            case 'setup':
-                # physically reset the board to the starting position
-                robot.setup_position('starting')
-                game = None
-            case 'clear':
-                # physically remove all pieces from the board
-                robot.setup_position('empty')
-                game = None
-            case 'adjust':
-                robot.adjust_pieces()
-            case 'home':
-                robot.home()
-            case 'exit':
-                sys.exit()
-            case _:
-                print('\tinvalid command')
+    GameManager(robot).run()
